36037782daa670fa6e6d30363746a804689ab580
Turkan Esgerova <turkan_1105@mail.ru>
2022-07-27 17:03:17 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+
+const App = () => {
+     return (
+          <>
+               <div className='div'>
+                    <div className='container'  >
+                         <h1 className='h1'>Newsletter</h1>
+                         <h3 className='h3'>Your Title of the news here</h3>
+                         <p className='p'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                              sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                              s erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation.
+                              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy
+                              nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                         <div className='flex' >
+                              <div>
+                                   <img className='img1' src='https://img.freepik.com/free-photo/little-girl-child-runs-green-grass-field-sunny-spring-weather-smile-joy-child-blue-sky-with-clouds_173815-6073.jpg?w=740'></img>
+                                   <img className='img1' src='https://content.active.com/Assets/Active.com+Content+Site+Digital+Assets/Kids/Galleries/Make+Running+Fun/Slide+1.jpg'></img>
+                              </div>
+                              <div><img className='img3' src='https://cdn-ds.com/blogs-media/sites/549/2018/07/29145118/child-smiling-in-pool_b.jpg'></img>
+                              </div>
+                         </div>
+                         <h2 className='h2'>ABOUT COVEREGE TOPIC</h2>
+                         <div className='flex' >
+                              <div className='part'>
+                                   <h3 className='part01'>Part 01</h3>
+                                   <p className='p'>Lorem ipsum dolor sit amet,
+                                        consectetuer adipiscing elit,
+                                        sed diam nonummy nibh euismod tincidunt
+                                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                                   <h3 className='part02'>Part 01</h3>
+                                   <p className='p'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                        sed diam nonummy nibh euismod tincidunt ut
+                                        laoreet dolore magna aliquam erat volutpat.</p>
+                              </div>
+                              <div className='part'>
+                                   <h3 className='part01'> Part 02</h3>
+                                   <p className='p'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                                   <h3 className='part02'>Part 02</h3>
+                                   <p className='p'>Lorem ipsum dolor sit amet,
+                                        consectetuer adipiscing elit,
+                                        sed diam nonummy nibh euismod tincidunt
+                                        ut laoreet dolore magna aliquam erat volutpat.</p>
+                              </div>
+
+
+
+                         </div>
+
+                    </div>
+
+               </div>
+
+
+
+
+
+
+
+
+
+
+
+          </>
+     )
+}
+
+export default App
